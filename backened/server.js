f27ce900a58ff97d4fe43016fd36298077af7e2f
GeetanjaliPersonal <geetanjalisingh1815@gmail.com>
2026-01-11 01:54:08 +0530

    cors issue

--- backened/server.js
+++ backened/server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
+const cors = require("cors");
 const app = require("./src/app");
 const seedProducts = require("./src/utils/seedProducts");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8007;
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
--- backened/src/app.js
+++ backened/src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -8,13 +7,7 @@ const app = express();
 connectDB();
 
 // middlewares
-const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
