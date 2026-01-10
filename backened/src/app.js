97f76c49cbbd13f1d80225689f8f4e93dc330bb8
GeetanjaliPersonal <geetanjalisingh1815@gmail.com>
2026-01-11 01:49:15 +0530

    cors issue

--- backened/src/app.js
+++ backened/src/app.js
@@ -8,10 +8,13 @@ const app = express();
 connectDB();
 
 // middlewares
-app.use(cors({
-    origin: true,  // dynamic origin, allows any requesting site
-    credentials: true
-}));
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
