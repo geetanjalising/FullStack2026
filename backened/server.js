619484268f12142245633f3bd4453e5ec9e5fddf
GeetanjaliPersonal <geetanjalisingh1815@gmail.com>
2026-01-11 02:02:40 +0530

    cors issue

--- backened/server.js
+++ backened/server.js
@@ -5,10 +5,16 @@ const seedProducts = require("./src/utils/seedProducts");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8007;
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173", // use your actual domain name (or localhost), using * is not recommended
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true
-}));
+}))
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
