7c11078e666bebd3c1c49f4b04a2426e708bab33
GeetanjaliPersonal <geetanjalisingh1815@gmail.com>
2026-01-11 11:28:18 +0530

    cors issue

--- backened/server.js
+++ backened/server.js
@@ -10,10 +10,9 @@ const PORT = process.env.PORT || 8007;
 //   credentials: true
 // }));
 app.use(cors({
-  origin: "http://localhost:5173", // use your actual domain name (or localhost), using * is not recommended
+  origin: "https://shopping2026.netlify.app/", // use your actual domain name (or localhost), using * is not recommended
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-  credentials: true
 }))
 
 app.listen(PORT, () => {
